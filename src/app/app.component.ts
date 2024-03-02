73db1c3e25c0b379a85e7493a28d2a34d243fc3c
Rafael Abdyev <abdyev_r@mail.ru>
2024-03-02 12:00:20 +0500

    add side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {
+	IonApp,
+	IonRouterOutlet,
+	IonSplitPane,
+	IonLabel,
+	IonIcon,
+	IonMenuToggle,
+	IonList, IonContent
+} from '@ionic/angular/standalone';
+import { IonicModule } from "@ionic/angular";
+import { RouterLink } from "@angular/router";
+import { CommonModule } from "@angular/common";
 
 @Component({
 	selector: 'app-root',
 	templateUrl: 'app.component.html',
 	standalone: true,
-	imports: [IonApp, IonRouterOutlet],
+	imports: [RouterLink, IonicModule, CommonModule],
 })
 export class AppComponent {
-	constructor() {
-	}
+	public links = [
+		{ title: 'Профиль', url: '/profile', icon: 'person-outline' },
+		{ title: 'Курсы', url: '/tracks', icon: 'file-tray-full-outline' },
+	];
 }
